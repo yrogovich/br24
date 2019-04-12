87f264f65b8f4fc8995eae9d8ff37159a2716a9c
Yaroslav <yRogovich@gmail.com>
2019-04-12 15:37:06 +0300

    First version site withot popups

--- src/js/main.js
+++ src/js/main.js
@@ -83,6 +83,23 @@
             $(".mobile-navbar").toggleClass("active");
             $('body').toggleClass("overflow");     
         });
+
+
+        var carousel = $("#slider").waterwheelCarousel({
+            // include options like this:
+            // (use quotes only for string values, and no trailing comma after last option)
+            // option: value,
+            // option: value
+            separation: 85,
+            keyboardNav: true
+        });
+
+        $(".arrow-left").click(function(){
+            carousel.next()
+        });
+        $(".arrow-right").click(function(){
+            carousel.prev()
+        });
     });
 
 })(jQuery); // <----- jQuery no conflict wrapper
@@ -143,3 +160,32 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #efefef}";
     document.body.appendChild(css);
 };
+
+( function() {
+
+	var youtube = document.querySelectorAll( ".youtube" );
+	
+	for (var i = 0; i < youtube.length; i++) {
+		
+		var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+		
+		var image = new Image();
+				image.src = source;
+				image.addEventListener( "load", function() {
+					youtube[ i ].appendChild( image );
+				}( i ) );
+		
+				youtube[i].addEventListener( "click", function() {
+
+					var iframe = document.createElement( "iframe" );
+
+							iframe.setAttribute( "frameborder", "0" );
+							iframe.setAttribute( "allowfullscreen", "" );
+							iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+							this.innerHTML = "";
+							this.appendChild( iframe );
+				} );	
+	};
+	
+} )();
--- templates/main.php
+++ templates/main.php
@@ -278,9 +278,174 @@ get_header();
             <!-- /.case-card -->
         </div>
         <!-- /.cases-container -->
+        <div class="text-center">
+            <a href="#" class="btn btn-with-case">Перейти в портфолио</a>
+        </div>
     </div>
 </div>
 <!-- /#cases.section -->
+<div class="section lazy" id="reviews" data-src="<?php bloginfo('template_url') ?>/src/img/main-page/mountains-bg.jpg">
+    <div class="container">
+        <div class="h2">Отзывы</div>
+        <div class="h2-primary">Что о нас говорят?</div>
+
+        <div class="rewiews">
+            <div class="rewiew">
+                <div class="pair">
+                    <div>
+                        <div class="title">ООО «Глонасс Северо-Запад»</div>
+                        <a href="http://glonassnw.ru/" class="link" target="_blank">glonassnw.ru</a>
+                    </div>
+                    <div class="navigation">
+                        <div class="arrow-left"></div>
+                        <div class="pair-2">
+                            <div class="digits">
+                                <div class="digit">1</div>
+                                 /8
+                                
+                            </div>
+                            <div class="text">
+                                отзыв
+                                </div>
+                        </div>
+                        <div class="arrow-right"></div>
+                    </div>
+                </div>
+                <div class="paragraph">
+                    <img src="<?php bloginfo('template_url') ?>/src/img/icons/quote.svg" alt="quote" class="quote">
+                    <p>ООО «Глонасс Северо-Запад» выражает искреннюю признательность компании Бизнес Рост за оперативное создание посадочной страницы (Landing Page) для нашей организации. Хотели бы поблагодарить за высокий профессионализм, творческий подход. Все наши пожелания были учтены и реализованы, несмотря на то, что наша компания предоставляет довольно специфические услуги, где непросто разобраться. Был очень грамотно проведен маркетинговый аудит, вследствие чего, нам практически не пришлось вносить корректировки. Работа была выполнена оперативно и качественно.</p>
+                </div>
+            </div>
+            <div id="slider">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/reviews/biznesrost-sertificate-1.jpg" alt="biznesrost-sertificate-1" class="lazy">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/reviews/biznesrost-sertificate-2.jpg" alt="biznesrost-sertificate-2" class="lazy">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/reviews/biznesrost-sertificate-3.jpg" alt="biznesrost-sertificate-3" class="lazy">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/reviews/biznesrost-sertificate-4.jpg" alt="biznesrost-sertificate-4" class="lazy">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/reviews/biznesrost-sertificate-5.jpg" alt="biznesrost-sertificate-5" class="lazy">
+            </div>
+        </div>
+
+        <div class="videos">
+            <div class="video-card">
+                <div class="youtube" data-embed="AqcjdkPMPJA">
+                    <div class="play-button"></div>
+                </div>
+                <div class="text">
+                    <div class="title">Иванов Павел</div>
+                    <div class="pair">
+                        <p class="gray-text">Руководитель группы компаний «Гефест Капитал»</p>
+                        <a href="https://www.gefestcapital.ru/" class="link" target="_blank">gefestcapital.ru</a>
+                    </div>
+                </div>
+            </div>
+            <div class="video-card">
+                <div class="youtube" data-embed="AqcjdkPMPJA">
+                    <div class="play-button"></div>
+                </div>
+                <div class="text">
+                    <div class="title">Иванов Павел</div>
+                    <div class="pair">
+                        <p class="gray-text">Руководитель группы компаний «Гефест Капитал»</p>
+                        <a href="https://www.gefestcapital.ru/" class="link" target="_blank">gefestcapital.ru</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="h3 h3-style-2">Нам доверяют</div>
+        <div class="brands-grid">
+            <div class="brand brand-1">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/bosh.png" alt="bosh" class="lazy">
+            </div>
+            <div class="brand brand-2">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/citroen.png" alt="citroen" class="lazy">
+            </div>
+            <div class="brand brand-3">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/tricolor.png" alt="tricolor" class="lazy">
+            </div>
+            <div class="brand brand-4">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/gefest.png" alt="gefest" class="lazy">
+            </div>
+            <div class="brand brand-5">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/nettlebox.png" alt="nettlebox" class="lazy">
+            </div>
+            <div class="brand brand-6">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/radamir.png" alt="radamir" class="lazy">
+            </div>
+            <div class="brand brand-7">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/rivera.png" alt="rivera" class="lazy">
+            </div>
+            <div class="brand brand-8">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/avon.png" alt="avon" class="lazy">
+            </div>
+            <div class="brand brand-9">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/palas_copy.png" alt="palas_copy" class="lazy">
+            </div>
+            <div class="brand brand-10">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/belinka.png" alt="belinka" class="lazy">
+            </div>
+            <div class="brand brand-11">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/hdlt.png" alt="hdlt" class="lazy">
+            </div>
+            <div class="brand brand-12">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/avtospas.png" alt="avtospas" class="lazy">
+            </div>
+            <div class="brand brand-13">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/glonas.png" alt="glonas" class="lazy">
+            </div>
+            <div class="brand brand-14">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/rancio.png" alt="rancio" class="lazy">
+            </div>
+            <div class="brand brand-15">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/cp.png" alt="cp" class="lazy">
+            </div>
+            <div class="brand brand-16">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/aurora.png" alt="aurora" class="lazy">
+            </div>
+            <div class="brand brand-17">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/limadas.png" alt="limadas" class="lazy">
+            </div>
+            <div class="brand brand-18">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/arkizu.png" alt="arkizu" class="lazy">
+            </div>
+            <div class="brand brand-19">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/neva.png" alt="neva" class="lazy">
+            </div>
+            <div class="brand brand-20">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/alfa.png" alt="alfa" class="lazy">
+            </div>
+            <div class="brand brand-21">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/germes.png" alt="germes" class="lazy">
+            </div>
+            <div class="brand brand-22">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/lord.png" alt="lord" class="lazy">
+            </div>
+            <div class="brand brand-23">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/divi.png" alt="divi" class="lazy">
+            </div>
+            <div class="brand brand-24">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/gidvokruf.png" alt="gidvokruf" class="lazy">
+            </div>
+            <div class="brand brand-25">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/odv.png" alt="odv" class="lazy">
+            </div>
+            <div class="brand brand-26">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/agronovia.png" alt="agronovia" class="lazy">
+            </div>
+            <div class="brand brand-27">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/rosstrans.png" alt="rosstrans" class="lazy">
+            </div>
+            <div class="brand brand-28">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/art.png" alt="art" class="lazy">
+            </div>
+            <div class="brand brand-29">
+                <img data-src="<?php bloginfo('template_url') ?>/src/img/brands/aresa-logo.png" alt="aresa-logo" class="lazy">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /#reviews.section -->
+
 <div class="section lazy" id="about-company" data-src="<?php bloginfo('template_url') ?>/src/img/main-page/br-background.jpg">
     <div class="container">
         <div class="h3">Коротко о нашей компании</div>
